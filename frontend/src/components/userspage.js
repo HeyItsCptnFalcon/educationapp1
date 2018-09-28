3c100d0f93f13d46f20144386eeb7235f67dd865
HeyItsCptnFalcon <cptnfalcon92@gmail.com>
2018-09-28 13:30:30 -0700

    added gitignore

--- frontend/src/components/userspage.js
+++ frontend/src/components/userspage.js
@@ -1,16 +1,1 @@
 import React from 'react';
-.
-
-
-
-class  extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
-  render() { 
-    return (  );
-  }
-}
- 
-export default ;
